9b83013cb694aa859ba425f4568f462883345ab1
Yagavuchance <yagavuchance@gmail.com>
2026-03-12 15:02:08 +0300

    Redesign portfolio layout

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu');
-
-    menuToggle.addEventListener('click', function() {
-        menu.classList.toggle('active');
-    });
-});
